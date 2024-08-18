5a0d3e36bdadab496974266d43a690751171ef70
Huguito999 <123948059+Huguito999@users.noreply.github.com>
2024-08-18 19:44:57 +0200

    5.5

--- bloglist-frontend/src/components/BlogForm.jsx
+++ bloglist-frontend/src/components/BlogForm.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export default function BlogForm({
   handleCreateBlog,
   newTitle,
@@ -7,36 +9,51 @@ export default function BlogForm({
   setNewAuthor,
   setNewUrl,
 }) {
+  const [blogFormVisible, setBLogFormVisible] = useState(false);
+
+  const hideWhenVisible = { display: blogFormVisible ? "none" : "" };
+  const showWhenVisible = { display: blogFormVisible ? "" : "none" };
+
   return (
-    <form onSubmit={handleCreateBlog}>
-      <div>
-        title
-        <input
-          type="text"
-          value={newTitle}
-          name="title"
-          onChange={({ target }) => setNewTitle(target.value)}
-        />
+    <div>
+      <div style={hideWhenVisible}>
+        <button onClick={() => setBLogFormVisible(true)}>Create Blog</button>
       </div>
-      <div>
-        author
-        <input
-          type="text"
-          value={newAuthor}
-          name="author"
-          onChange={({ target }) => setNewAuthor(target.value)}
-        />
+      <div style={showWhenVisible}>
+        <form onSubmit={handleCreateBlog}>
+          <div>
+            title
+            <input
+              type="text"
+              value={newTitle}
+              name="title"
+              onChange={({ target }) => setNewTitle(target.value)}
+            />
+          </div>
+          <div>
+            author
+            <input
+              type="text"
+              value={newAuthor}
+              name="author"
+              onChange={({ target }) => setNewAuthor(target.value)}
+            />
+          </div>
+          <div>
+            url
+            <input
+              type="text"
+              value={newUrl}
+              name="url"
+              onChange={({ target }) => setNewUrl(target.value)}
+            />
+          </div>
+          <button type="submit">Create</button>
+        </form>
+       
+          <button onClick={() => setBLogFormVisible(false)}>Cancel</button>
+        
       </div>
-      <div>
-        url
-        <input
-          type="text"
-          value={newUrl}
-          name="url"
-          onChange={({ target }) => setNewUrl(target.value)}
-        />
-      </div>
-      <button type="submit">Create</button>
-    </form>
+    </div>
   );
 }
